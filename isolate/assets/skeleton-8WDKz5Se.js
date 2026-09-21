4489db038fb789fab6b3a5515295df5d0522c0f2
Hứa Tiến Dương <329364602+huatienduong@users.noreply.github.com>
2026-09-21 18:16:47 +0000

    Before: https://dharma.freebuff.app/ truy cập không hiển thị nội dung fix lỗi ngay lập tức

--- isolate/assets/skeleton-8WDKz5Se.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./framer-motion-DCqQklih.js";import{c as s}from"./index-V4EUe2CJ.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
