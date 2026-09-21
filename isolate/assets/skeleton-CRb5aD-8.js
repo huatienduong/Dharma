193f3c68f4ab109e948d492f68a4e06bf115952f
Hứa Tiến Dương <329364602+huatienduong@users.noreply.github.com>
2026-09-21 19:10:55 +0000

    Before: Nâng cấp phiên bản mới với nhiều cải tiến: Loại bỏ hoàn toàn chế độ đăng nhập và đăng ký. Toàn bộ dữ...

---
+++ isolate/assets/skeleton-CRb5aD-8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./framer-motion-DCqQklih.js";import{e as s}from"./index-a7dhgcsK.js";function r({className:e,...t}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",e),...t})}export{r as S};
--- isolate/assets/skeleton-CgeTgeL6.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./framer-motion-DCqQklih.js";import{e as s}from"./index-C868G-Hk.js";function r({className:e,...t}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",e),...t})}export{r as S};
