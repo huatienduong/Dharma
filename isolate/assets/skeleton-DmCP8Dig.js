b7a8bc8a167110d6ab03a36e63dd7a7b59c587a4
Hứa Tiến Dương <329364602+huatienduong@users.noreply.github.com>
2026-09-22 00:36:17 +0000

    Production deployment preparation for Dharma

---
+++ isolate/assets/skeleton-DmCP8Dig.js
@@ -0,0 +1,1 @@
+import{j as o}from"./framer-motion-DCqQklih.js";import{f as s}from"./index-C2p0HCXf.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
