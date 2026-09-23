3f4c46e2f8c8044842a726f6483d55f914dada49
Hứa Tiến Dương <329364602+huatienduong@users.noreply.github.com>
2026-09-23 20:55:48 +0000

    Before:  Cải thiện lại giao diện ứng dụng: 1. Đổi giao diện thành màu theo truyền thống Theravada. 2. Xóa bỏ...

--- isolate/assets/chevron-left-D-8CuXo9.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./index-CP0INxLB.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],c=e("chevron-left",o);export{c as C};
---
+++ isolate/assets/chevron-left-DLIr6A5b.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-BA9RpqQ5.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],c=e("chevron-left",o);export{c as C};
