3972e66a3ea21c4163d10cce3ba9b8ec50e855ba
Hứa Tiến Dương <329364602+huatienduong@users.noreply.github.com>
2026-09-23 19:10:28 +0000

    Before: Xóa bỏ nút tiến trình của tôi trên trang chủ. Mục tin tức, lịch sử Phật giáo, Lịch sử đã xem cho đồn...

--- isolate/assets/skeleton-CKmjnl5k.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./framer-motion-C_cneupK.js";import{d as s}from"./index-fxJHhIZn.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
